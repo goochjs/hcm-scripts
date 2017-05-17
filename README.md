d048607802bd2dc3d6b631b896f2b6ca206d37d2
Jeremy Gooch <goochjs@yahoo.co.uk>
2017-05-17 14:17:05 +0100

    First complete version

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
-# myHR scripts
+# Oracle HCM scripts
 
 These scripts automate the extraction and monitoring of Oracle HCM data.
 
 - One curl call is made to trigger a data extraction
 - A second curl call is repeatedly made to monitor the status of the extraction
 
+The success of the script can be used as a trigger of a subsequent file transfer.
+
+
 ## Parameters
 
+- -j  HCM job name
 - -p  password
+- -s  host server endpoint url (inc https)
 - -u  username
+
 
 ## Return codes
 
 - 00 - if the extraction completes successfully
-- 05 - if the extraction does complete before the script ends
+- 05 - if the extraction does not complete before the script ends
 - 09 - if the call to HCM's APIs fails
 - 99 - all other errors
 
+
 ## Docker
 
-A dockerfile is included, to allow development and testing of the scripts.
+A dockerfile is included, to allow development and testing of the scripts on Windows.
 
 This is based on a CentOS image, as it is assumed that the target environment is RedHat-based.
 
@@ -29,4 +36,4 @@ This is based on a CentOS image, as it is assumed that the target environment is
 
 ### Run
 
-    docker run -it --rm --name hcm-scripts_1 hcm-scripts **PARAMETERS**
+    docker run -it --rm --name hcm-scripts_1 hcm-scripts -u $username -p $password -s $server
--- bin/hcm-extract.sh
+++ bin/hcm-extract.sh
@@ -7,18 +7,34 @@
 # configuration stuff ----------------
 
 _DEBUG="off"
+
+## curl control
 SCRIPTDIR=$(dirname $0)
 SCRIPTNAME=$(basename $0)
-LOGDIR="/tmp/${SCRIPTNAME}/log"
+REQUEST_DIR="/tmp/${SCRIPTNAME}/request"
+RESPONSE_DIR="/tmp/${SCRIPTNAME}/response"
 HEADERS="Content-Type:text/xml;charset=UTF-8"
-REQUEST_FILE=${SCRIPTDIR}/../cfg/submitFlow.xml
+SUBMIT_FILE_ORIG=${SCRIPTDIR}/../cfg/hcm-submit.xml
+SUBMIT_FILE=${REQUEST_DIR}/hcm-submit_$(date +%Y%m%d%H%M%S).xml
+STATUS_FILE_ORIG=${SCRIPTDIR}/../cfg/hcm-status.xml
+STATUS_FILE=${REQUEST_DIR}/hcm-status_$(date +%Y%m%d%H%M%S).xml
 CONNECT_TIMEOUT=120
 MAX_TIME=3600
 URL_PATH="hcmProcFlowCoreController/FlowActionsService"
 
-POLL_INTERVAL=5
-POLL_COUNT=5
+## monitor loop control
+POLL_INTERVAL=60
+POLL_COUNT=10
 
+## request file configuration literals
+## these strings are in ${SUBMIT_FILE_ORIG} and ${STATUS_FILE_ORIG}
+LIT_JOBNAME="JOBNAME"
+LIT_DATESTAMP="DATESTAMP"
+LIT_TIMESTAMP="TIMESTAMP"
+## these are the strings to be searched for in the status output
+LIT_COMPLETE="COMPLETED"
+
+## return codes
 RC_SUCCESS=0
 RC_USAGE=1
 RC_TIMEOUT=5
@@ -26,51 +42,87 @@ RC_HTTP_ERROR=9
 RC_GENERAL_ERROR=99
 
 
-# commands used ----------------------
-
-
-
-
 # functions --------------------------
 
 
 usage ()
 {
-  echo "$0 [-h] -- Oracle HCM data extract script
+  echo "$0 [params] -- Oracle HCM data extract script
 
-where
+  - One curl call is made to trigger a data extraction
+  - A second curl call is repeatedly made to monitor the status of the extraction
+
+  The success of the script can be used as a trigger of a subsequent file transfer.
+
+params are
     -h  show this help message
+    -j  HCM job name
     -p  password
+    -s  host server endpoint url (inc https)
     -u  username"
 
   exit ${RC_USAGE}
 }
 
 
+prepare_config_file ()
+{
+  # Take the original configuration file and add the appropriate date, time and job name
+  # This function takes three parameters
+  #    1 = whether it is the submission or the status job that's being configured
+  #    2 = the original config file (which contains literals in need of substitution)
+  #    3 = the output file into which config will be written with literals substituted
+  # The output file will be fed to HCM
+
+  if [ ! -f ${2} ]; then
+      log "${1} configuration file ${2} not found!"
+      exit ${RC_GENERAL_ERROR}
+  fi
+
+  log "Original ${1} configuration file ${2}"
+
+  mkdir -p ${REQUEST_DIR} >/dev/null 2>&1
+  cp ${2} ${3}
+
+  sed -i -e "s/${LIT_JOBNAME}/${JOBNAME}/g" ${3}
+  sed -i -e "s/${LIT_DATESTAMP}/$(date +%Y-%m-%d)/g" ${3}
+  sed -i -e "s/${LIT_TIMESTAMP}/$(date +%H%M%S)/g" ${3}
+}
+
+
 call_hcm ()
 {
+  # Call the HCM web service
+  # This function takes two parameters
+  #    1 = an input file to put into the HTTP request
+  #    2 = the output file into which the HTTP response will be written
+
   USER_AUTH="${USERNAME}:${PASSWORD}"
   URL="${HOST_SERVER}/${URL_PATH}"
-  OUTPUT="${LOGDIR}/${SCRIPTNAME}_$(date +%Y%m%d%H%M%S).gz"
 
+  log "Request file ${1}"
+  log "Response file ${2}"
   log "Calling ${URL}"
-  log "Output file is ${OUTPUT}"
 
-  mkdir -p ${LOGDIR} >/dev/null 2>&1
+  if [ ! -f ${1} ]; then
+      log "Request file not found ${1}"
+      exit ${RC_GENERAL_ERROR}
+  fi
 
   # set up the curl command
-  CURL="curl -w %{http_code} --silent --user ${USER_AUTH} --header ${HEADERS} -o ${OUTPUT} --data @${REQUEST_FILE} --connect-time ${CONNECT_TIMEOUT} --max-time ${MAX_TIME} ${URL}"
+  CURL="curl -w %{http_code} --silent --user ${USER_AUTH} --header ${HEADERS} -o ${2} --data @${1} --connect-time ${CONNECT_TIMEOUT} --max-time ${MAX_TIME} ${URL}"
 
   # run the curl command, which returns the response code
   HTTP_RESPONSE=$(${CURL})
 
-  log "HTTP response code ${HTTP_RESPONSE}"
-
   if [[ ${HTTP_RESPONSE} == 200 ]]; then
-      log "Successful call"
+      log "Successful call (${HTTP_RESPONSE})"
   else
-      log "ERROR - SOAP output follows"
-      gunzip -c ${OUTPUT}
+      log "HTTP response code ${HTTP_RESPONSE}"
+      log "ERROR - SOAP request follows"
+      cat ${1}
+      log "ERROR - SOAP response follows"
+      gunzip -c ${2}
       exit ${RC_HTTP_ERROR}
   fi
 }
@@ -102,8 +154,11 @@ if [ $# -eq 0 ] ; then
 fi
 
 # check command line params,
-while getopts ":p:s:u:" opt; do
+while getopts ":j:p:s:u:" opt; do
   case ${opt} in
+    j)
+      JOBNAME=${OPTARG}
+    ;;
     p)
       PASSWORD=${OPTARG}
       ;;
@@ -119,22 +174,43 @@ while getopts ":p:s:u:" opt; do
   esac
 done
 
-if [ -z "${PASSWORD}" ] || [ -z "${HOST_SERVER}" ] || [ -z "${USERNAME}" ]; then
+if [ -z "${JOBNAME}" ] || [ -z "${PASSWORD}" ] || [ -z "${HOST_SERVER}" ] || [ -z "${USERNAME}" ]; then
     usage
 fi
 
-log "Started"
+log "${JOBNAME} started"
 
-call_hcm
+# prepare submission and status config files
+prepare_config_file submit ${SUBMIT_FILE_ORIG} ${SUBMIT_FILE}
+prepare_config_file status ${STATUS_FILE_ORIG} ${STATUS_FILE}
 
-COUNTER=0
-while [  ${COUNTER} -lt ${POLL_COUNT} ]; do
+mkdir -p ${RESPONSE_DIR} >/dev/null 2>&1
+
+# submit the HCM job
+call_hcm ${SUBMIT_FILE} ${RESPONSE_DIR}/${SCRIPTNAME}_submit_$(date +%Y%m%d%H%M%S).gz
+
+# call the HCM status job repeatedly, exiting if complete
+COUNTER=1
+while [ ${COUNTER} -le ${POLL_COUNT} ]; do
+  log "Waiting ${POLL_INTERVAL} seconds..."
   sleep ${POLL_INTERVAL}
-  echo The counter is ${COUNTER}
+  log "Status check: ${COUNTER} of ${POLL_COUNT}"
+
+  RESPONSE_FILE=${RESPONSE_DIR}/${SCRIPTNAME}_status_$(date +%Y%m%d%H%M%S).gz
+  call_hcm ${STATUS_FILE} ${RESPONSE_FILE}
+
+  if grep -q ${LIT_COMPLETE} "${RESPONSE_FILE}"; then
+     log "Job ${JOBNAME} successfully completed"
+     exit ${RC_SUCCESS}
+  fi
+
+  log "${JOBNAME} not yet complete"
   let COUNTER=COUNTER+1
 done
 
-log "Finished"
+log "ERROR - last SOAP response follows"
+cat ${RESPONSE_FILE}
+log "${JOBNAME} did not complete before timeout"
 
 DEBUG set +x
-exit ${RC_SUCCESS}
+exit ${RC_TIMEOUT}
